6b85793dc22a7224fe8b5efdec81037c7c9c95d4
Ludwe Sokani <ludwesokani@gmail.com>
2025-11-26 13:51:34 +0200

    Imported favorite and date functions along with needed react ones

--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -1,1 +1,5 @@
+import { getfavorites, togglefavorite } from "../utils/favorites.js";
+import { formatDateAndTime } from "../utils/formatDate.js";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
