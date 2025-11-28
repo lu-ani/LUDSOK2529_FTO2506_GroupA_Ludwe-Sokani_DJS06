b8a71193db1d9cec735f42505d86624f17c16ce9
Ludwe Sokani <ludwesokani@gmail.com>
2025-11-28 11:44:22 +0200

    Variables needed for this file

--- src/components/player/AudioPlayerProvider.jsx
+++ src/components/player/AudioPlayerProvider.jsx
@@ -1,1 +1,12 @@
 
+/**
+ * @typedef {Object} EpisodeData
+ * @property {string} showId - The podcast show ID.
+ * @property {number} season - The season number.
+ * @property {number} episodeNumber - The episode number.
+ * @property {string} title - The episode title.
+ * @property {string} audioUrl - Audio stream URL.
+ * @property {string} image - Episode or season artwork.
+ * @property {string} showTitle - Podcast show title.
+ */
+const EPISODE_REGISTRY = {};
