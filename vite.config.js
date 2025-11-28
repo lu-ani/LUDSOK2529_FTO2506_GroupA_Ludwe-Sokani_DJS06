8a77251e8acc8877e9d0493ca6532872168be08f
Ludwe Sokani <ludwesokani@gmail.com>
2025-11-28 17:11:10 +0200

    vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base:
+    process.env.VITE_BASE_PATH ||
+    "/LUDSOK2529_FTO2506_GroupA_Ludwe-Sokani_DJS06",
+});
